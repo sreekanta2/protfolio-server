3212531cbd894727aafe0cf3062fb808d710ea57
sreekanta2 <sreekanta03s@gmail.com>
2023-01-11 21:36:00 +0600

    eh

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const nodemailer = require("nodemailer");
 
 // server used to send send emails
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+
+    methods: ["GET", "POST", "DELETE"],
+}));
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
